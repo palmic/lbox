749ba165516021c763118d46db0e6b42e0eaa0ac
Michal Palma <michal.palma@gmail.com>
2008-07-25 15:05:30 +0200

    Opraveno par bugu Forms a dodany nejake dalsi funkcnosti

--- project/classes/forms/exceptions/class.LBoxExceptionFormProcessorLogin.php
+++ project/classes/forms/exceptions/class.LBoxExceptionFormProcessorLogin.php
@@ -4,9 +4,9 @@
  */
 class LBoxExceptionFormProcessor extends LBoxExceptionForm
 {
-	const CODE_FORM_PROCESSOR_			= 18001;
+	const CODE_FORM_PROCESSOR_LOGIN_UNCORRECT			= 18101;
 	
-	const MSG_FORM_PROCESSOR_			= "";
+	const MSG_FORM_PROCESSOR_LOGIN_UNCORRECT			= "Login uncorrect";
 	
 }
 ?>
---
+++ project/classes/forms/processors/abstract.ProcessorRegistration.php
@@ -0,0 +1,20 @@
+<?php
+class ProcessorRegistration extends LBoxFormProcessor
+{
+	public function process() {
+		try {
+			$controls	= $this->form->getSentDataByControlName("email");
+			$records	= new XTUsersRecords(array(
+													"email" 	=> $this->form->getSentDataByControlName("email"),
+													"password" => $this->form->getSentDataByControlName("password"),
+			));
+			if ($records->count() < 1) {
+				throw new LBoxExceptionFormProcessor();
+			}
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+}
+?>
