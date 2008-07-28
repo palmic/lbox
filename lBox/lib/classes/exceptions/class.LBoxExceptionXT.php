e0232ff25ab3de20a97845a24a44f15a06c9592b
Michal Palma <michal.palma@gmail.com>
2008-07-28 16:17:01 +0200

    update LBoxExceptionXT

--- lBox/lib/classes/exceptions/class.LBoxExceptionXT.php
+++ lBox/lib/classes/exceptions/class.LBoxExceptionXT.php
@@ -12,10 +12,12 @@ class LBoxExceptionXT extends LBoxException
 	const CODE_NOT_LOGGED 										= 10002;
 	const CODE_ROLE_NOT_EXISTS									= 10003;
 	const CODE_USER_NOT_CONFIRMED								= 10004;
+	const CODE_USER_CONFIRMED									= 10005;
 	
 	const MSG_LOGIN_INVALID 									= "Login nick or password invalid";	
 	const MSG_NOT_LOGGED 										= "User is not signed on!";	
 	const MSG_ROLE_NOT_EXISTS									= "XT role does not exists in system!";	
 	const MSG_USER_NOT_CONFIRMED								= "User registration not confirmed!";
+	const MSG_USER_CONFIRMED									= "User registration already confirmed!";
 }
 ?>
