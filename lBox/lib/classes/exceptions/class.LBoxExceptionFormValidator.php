a34774161f6edd042341a97d1f8cdabc8d901515
Michal Palma <michal.palma@gmail.com>
2008-07-29 16:06:34 +0200

    Update Exceptions

--- lBox/lib/classes/exceptions/class.LBoxExceptionFormValidator.php
+++ lBox/lib/classes/exceptions/class.LBoxExceptionFormValidator.php
@@ -8,10 +8,12 @@ class LBoxExceptionFormValidator extends LBoxExceptionForm
 	const CODE_FORM_VALIDATION_PASSWORDS_NOTSAME		= 17002;
 	const CODE_FORM_VALIDATION_EMAIL_NOTVALID			= 17003;
 	const CODE_FORM_VALIDATION_SPAMDEFENSEJS_NOT_PASS	= 17004;
+	const CODE_FORM_VALIDATION_FILE_NOT_IMAGE			= 17005;
 	
 	const MSG_FORM_VALIDATION_CONTROL_EMPTY				= "required control is empty";
 	const MSG_FORM_VALIDATION_PASSWORDS_NOTSAME			= "passwords do not match";
 	const MSG_FORM_VALIDATION_EMAIL_NOTVALID			= "email is not valid";
 	const MSG_FORM_VALIDATION_SPAMDEFENSEJS_NOT_PASS	= "spam defense does not pass";
+	const MSG_FORM_VALIDATION_FILE_NOT_IMAGE			= "file is not image";
 }
 ?>
