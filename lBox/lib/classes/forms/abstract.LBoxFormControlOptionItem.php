bf4d2e4af41bffdc46dc4128aa97d76b28cf5fab
Michal Palma <michal.palma@gmail.com>
2008-07-29 16:21:57 +0200

    Update Forms

---
+++ lBox/lib/classes/forms/abstract.LBoxFormControlOptionItem.php
@@ -0,0 +1,92 @@
+<?php
+abstract class LBoxFormControlOptionItem extends LBoxFormControlChoose
+{
+	/**
+	 * hodnota option, ktera bude odeslana formularem v pripade, ze tato option bude zvolena
+	 * @var string
+	 */
+	protected $value	= "";
+
+	/**
+	 * hodnota option, ktera bude zobrazena uzivateli
+	 * @var string
+	 */
+	protected $label	= "";
+	
+	/**
+	 * nadrazeny control
+	 * @var LBoxFormControlChoose
+	 */
+	protected $control	= "";
+	
+	/**
+	 * @param string value 
+	 * @param string label 
+	 */
+	public function __construct( $value = "",  $label = "" ) {
+		try {
+			if (strlen($value) < 1) {
+				throw new LBoxExceptionFormControl("\$value: ". LBoxExceptionFormControl::MSG_PARAM_STRING_NOTNULL, LBoxExceptionFormControl::CODE_BAD_PARAM);
+			}
+			if (strlen($label) < 1) {
+				throw new LBoxExceptionFormControl("\$label: ". LBoxExceptionFormControl::MSG_PARAM_STRING_NOTNULL, LBoxExceptionFormControl::CODE_BAD_PARAM);
+			}
+			$this->value	= $value;
+			$this->label	= $label;
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+
+	/**
+	 * vraci true v pripade, ze byla tato volba zvolena
+	 * @return bool
+	 * @access public
+	 */
+	public function isSelected() {
+		try {
+			return ($this->getValue() == $this->control->getValue());
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getValue() {
+		try {
+			return $this->value;
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getLabel() {
+		try {
+			return $this->label;
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+
+	/**
+	 * nastavuje se vnitrne v LBoxFormControlChoose, nepouzivat z venci!
+	 * @param LBoxFormControlChoose control 
+	 */
+	public function setControl(LBoxFormControlChoose $control) {
+		try {
+			$this->control	= $control;
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+}
---
+++ lBox/lib/classes/forms/filters/class.FilterEraseSpaces.php
@@ -0,0 +1,19 @@
+<?php
+class FilterEraseSpaces extends LBoxFormFilter
+{
+	/**
+	 * regularni vyraz pro smazani mezer
+	 * @var string
+	 */
+	protected $regEraseSpace	= "[:space:]";
+	
+	public function filter(LBoxFormControl $control = NULL) {
+		try {
+			return ereg_replace($this->regEraseSpace, "", $control->getValue());
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+}
+?>
