f2f95687452edead70992390e5aea954fc185377
Michal Palma <michal.palma@gmail.com>
2008-07-29 15:29:15 +0200

    Update LBoxForms

--- lBox/lib/classes/exceptions/class.LBoxExceptionFormControl.php
+++ lBox/lib/classes/exceptions/class.LBoxExceptionFormControl.php
@@ -6,8 +6,12 @@ class LBoxExceptionFormControl extends LBoxExceptionForm
 {
 	const CODE_FORM_FILTER_OUT_VALUE_NULL		= 16001;
 	const CODE_FORM_CONTROL_VALUE_NOT_OPTION	= 16002;
+	const CODE_FORM_CONTROL_VALUE_SET_ALREADY	= 16003;
+	const CODE_FORM_CONTROL_FILE_UPLOAD_ERROR	= 16101;
 	
 	const MSG_FORM_FILTER_OUT_VALUE_NULL		= "form validator output value cannot be NULL";
 	const MSG_FORM_CONTROL_VALUE_NOT_OPTION		= "form control value sent is not in options";
+	const MSG_FORM_CONTROL_VALUE_SET_ALREADY	= "form control value already set in option";
+	const MSG_FORM_CONTROL_FILE_UPLOAD_ERROR	= "posible form upload attack detected!";
 }
 ?>
