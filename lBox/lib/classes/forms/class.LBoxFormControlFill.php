15d06e439ce732df32008c297d26254638853880
Michal Palma <michal.palma@gmail.com>
2008-08-06 14:27:47 +0200

    Pridana moznost definovat i ControlFill maxlength

--- lBox/lib/classes/forms/class.LBoxFormControlFill.php
+++ lBox/lib/classes/forms/class.LBoxFormControlFill.php
@@ -8,5 +8,40 @@ class LBoxFormControlFill extends LBoxFormControl
 	 * @var string
 	 */
 	protected $filenameTemplate = "lbox_form_control_fill.html";
+	
+	/**
+	 * maxlength html attribute value
+	 * @var int
+	 */
+	protected $lengthMax	= NULL;
+
+	/**
+	 * doplneno o parametr length
+	 */
+	public function __construct($name = "",  $label = "",  $default = "", $lengthMax	= NULL) {
+		try {
+			if ($lengthMax && (!is_numeric($lengthMax))) {
+				throw new LBoxExceptionFormControl(LBoxExceptionFormControl::MSG_PARAM_INT, LBoxExceptionFormControl::CODE_BAD_PARAM);
+			}
+			$this->lengthMax	= $lengthMax;
+			parent::__construct($name,  $label,  $default);
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+	
+	/**
+	 * getter na $lengthMax
+	 * @return int
+	 */
+	public function getLengthMax()	{
+		try {
+			return	$this->lengthMax;
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
 }
 ?>
