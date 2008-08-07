298b93dcf914dd37561f8bfb574ceaec4845ba5e
Michal Palma <michal.palma@gmail.com>
2008-08-07 11:57:17 +0200

    Upgrade talu na 1.1.13

--- lBox/lib/loader.php
+++ lBox/lib/loader.php
@@ -102,7 +102,7 @@ try {
 	// pokud nemame pearovsky PHPTAL pouzivame lokani LBOXovy
 	@include("PHPTAL.php");
 	if (!@constant("PHPTAL_VERSION")) {	
-		require(LBOX_PATH_CORE 			. $slash ."TAL". $slash ."PHPTAL-1.1.12". $slash ."PHPTAL.php");
+		require(LBOX_PATH_CORE 			. $slash ."TAL". $slash ."PHPTAL-1.1.13". $slash ."PHPTAL.php");
 	}
 }
 catch (Exception $e) {
