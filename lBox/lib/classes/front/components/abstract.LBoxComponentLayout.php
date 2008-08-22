ba63ca9672a55b5670a3abac51a6a0f65bb1bfde
Michal Palma <michal.palma@gmail.com>
2008-08-22 17:40:10 +0200

    Updatnuto parserovani v LBoxComponentLayout

--- lBox/lib/classes/front/components/abstract.LBoxComponentLayout.php
+++ lBox/lib/classes/front/components/abstract.LBoxComponentLayout.php
@@ -11,6 +11,27 @@ abstract class LBoxComponentLayout extends LBoxComponent
 {
 	const DEBUG = false;
 	
-	protected $templatePath = LBOX_PATH_TEMPLATES_LAYOUTS;	
+	protected $templatePath = LBOX_PATH_TEMPLATES_LAYOUTS;
+
+	/**
+	 * pridava parsing vysledku podle configu
+	 * @param string $out
+	 * @return string
+	 * @throws Exception
+	 */
+	public function getContent() {
+		try {
+			$out	= parent::getContent();
+			if ($this->isDebugOn()) {
+				return $out;
+			}
+			$out 	= $this->removeComents($out);
+			$out	= $this->compress($out);
+			return $out;
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
 }
 ?>
