d85809ab461de4cde3bdfc103fb02aeda6becee1
Michal Palma <michal.palma@gmail.com>
2008-08-26 14:11:58 +0200

    doplneny nove validatory forms

---
+++ lBox/lib/classes/forms/validators/class.LBoxFormValidatorBirthYear.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * zkontroluje, jestli hodnota odpovida pravdepodobnemu roku narozeni
+ */
+class LBoxFormValidatorBirthYear extends LBoxFormValidator
+{
+	/**
+	 * maximalni tolerovany vek
+	 * @var int
+	 */
+	protected $maxAge	= 100;
+	
+	public function validate(LBoxFormControl $control = NULL) {
+		try {
+			if (!$this->isValidBirthYear($control->getValue())) {
+				throw new LBoxExceptionFormValidatorsMaybelline(LBoxExceptionFormValidator::MSG_FORM_VALIDATION_BIRTHYEAR_NOTVALID,
+																LBoxExceptionFormValidator::CODE_FORM_VALIDATION_BIRTHYEAR_NOTVALID);
+			}
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+	
+	/**
+	 * Vraci, jestli je hodnotu mozno povazovat za rok narozeni
+	 * @param int $year
+	 * @return bool
+	 */
+	protected function isValidBirthYear($year	= 0) {
+		try {
+			if (!is_numeric($year)) {
+				return false;
+			}
+			if ($year < date("Y")-$this->maxAge) {
+				return false;
+			}
+			if ($year > date("Y")) {
+				return false;
+			}
+			return true;
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+}
+?>
---
+++ lBox/lib/classes/forms/validators/class.LBoxFormValidatorFileName.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * zkontroluje, jestli je nazev souboru validni (vuci kodovani atd)
+ */
+class LBoxFormValidatorFileName extends LBoxFormValidator
+{
+	/**
+	 * regularni vyraz kontrolujici vymezeni povolenych znaku v nazvu filu
+	 * @var string
+	 */
+	protected $regSigns	= "^([[:alnum:]_\.\-])+$";
+	
+	public function validate(LBoxFormControl $control = NULL) {
+		try {
+			$valueFiles	= $control->getValueFiles();
+			if (strlen($control->getValue()) > 0)
+			if (!ereg($this->regSigns, $valueFiles["name"])) {
+				throw new LBoxExceptionFormValidator(	LBoxExceptionFormValidator::MSG_FORM_VALIDATION_FILENAME_INVALID,
+														LBoxExceptionFormValidator::CODE_FORM_VALIDATION_FILENAME_INVALID);
+			}
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+}
+?>
