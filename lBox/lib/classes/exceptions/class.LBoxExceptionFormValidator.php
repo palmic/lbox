01591fc87d3cecc31eb6db162cf415559d825abd
Michal Palma <palmic@email.cz>
2008-09-07 19:38:00 +0200

    Upravena LBoxExceptionFormValidator

--- lBox/lib/classes/exceptions/class.LBoxExceptionFormValidator.php
+++ lBox/lib/classes/exceptions/class.LBoxExceptionFormValidator.php
@@ -11,7 +11,7 @@ class LBoxExceptionFormValidator extends LBoxExceptionForm
 	const CODE_FORM_VALIDATION_FILE_NOT_IMAGE			= 17005;
 	const CODE_FORM_VALIDATION_PHONE_NOTVALID			= 17006;
 	const CODE_FORM_VALIDATION_FILENAME_INVALID			= 17007;
-	const CODE_FORM_VALIDATION_CONTROL_VALUE_NOT_VALID	= 17008;
+	const CODE_FORM_VALIDATION_CONTROL_NOT_VALID		= 17008;
 	
 	const MSG_FORM_VALIDATION_CONTROL_EMPTY				= "required control is empty";
 	const MSG_FORM_VALIDATION_PASSWORDS_NOTSAME			= "passwords do not match";
@@ -20,6 +20,6 @@ class LBoxExceptionFormValidator extends LBoxExceptionForm
 	const MSG_FORM_VALIDATION_FILE_NOT_IMAGE			= "file is not image";
 	const MSG_FORM_VALIDATION_PHONE_NOTVALID			= "phone is not valid";
 	const MSG_FORM_VALIDATION_FILENAME_INVALID			= "filename contents invalid signs";
-	const MSG_FORM_VALIDATION_CONTROL_VALUE_NOT_VALID	= "Invalid value";
+	const MSG_FORM_VALIDATION_CONTROL_NOT_VALID			= "Invalid value";
 }
 ?>
