1377df4fc92144bf668a3e2225d5bde90ba787c0
Michal Palma <palmic@email.cz>
2008-09-07 23:25:09 +0200

    Upravena exception

--- lBox/lib/classes/exceptions/class.LBoxExceptionForm.php
+++ lBox/lib/classes/exceptions/class.LBoxExceptionForm.php
@@ -7,16 +7,35 @@
 */
 class LBoxExceptionForm extends LBoxException
 {
-	const CODE_FORM_CONTROL_DOESNOT_EXISTS		= 15001;
-	const CODE_FORM_CONTROL_DOES_EXISTS			= 15002;
-	const CODE_FORM_PROCESSOR_DOESNOT_EXISTS	= 15003;
-	const CODE_FORM_PROCESSOR_DOES_EXISTS		= 15004;
-	const CODE_FORM_DUPLICATE_FORMNAME			= 15005;
+	const CODE_FORM_DATA_INVALID		= 15101;
 	
-	const MSG_FORM_CONTROL_DOESNOT_EXISTS		= "form control does not exists in the form";
-	const MSG_FORM_CONTROL_DOES_EXISTS			= "this form control does already exists in form";
-	const MSG_FORM_PROCESSOR_DOESNOT_EXISTS		= "There are no form processor set";
-	const MSG_FORM_PROCESSOR_DOES_EXISTS		= "this form processor does already exists in form";
-	const MSG_FORM_DUPLICATE_FORMNAME			= "cannot create new form with already used name";
+	const MSG_FORM_DATA_INVALID			= "Form data invalid";
+	
+	protected $formArray				= array();
+
+	/**
+	 * form array setter
+	 * @param array $formArray
+	 * @throws LBoxException
+	 */
+	public function setFormArray ($formArray	= array()) {
+		try {
+			if (!is_array($formArray)) {
+				throw new LBoxException(LBoxException::MSG_PARAM_STRING_NOTNULL, LBoxException::CODE_BAD_PARAM);
+			}
+			$this->formArray	= $formArray;
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+
+	/**
+	 * form array getter
+	 * @return array
+	 */
+	public function getFormArray () {
+		return $this->formArray;
+	}
 }
 ?>
