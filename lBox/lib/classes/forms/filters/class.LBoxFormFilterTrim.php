5e417526d1462cf5f5f91f8ed5488c9722fce43e
Michal Palma <palmic@email.cz>
2008-09-07 14:18:29 +0200

    Pridan FilterTrim do forms

---
+++ lBox/lib/classes/forms/filters/class.LBoxFormFilterTrim.php
@@ -0,0 +1,13 @@
+<?php
+class LBoxFormFilterTrim extends LBoxFormFilter
+{
+	public function filter(LBoxFormControl $control = NULL) {
+		try {
+			return trim($control->getValue());
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+}
+?>
