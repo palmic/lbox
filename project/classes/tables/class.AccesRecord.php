36252744cea06b2ad2bf08453df38f4889da9651
Michal Palma <michal.palma@gmail.com>
2008-09-16 17:55:56 +0200

    Opravena javadoc

--- project/classes/tables/class.AccesRecord.php
+++ project/classes/tables/class.AccesRecord.php
@@ -2,7 +2,7 @@
 /**
  * Pozor! pouziva se jako singleton presto ze ma public constructor - kuli dedicnosti
 * @author Michal Palma <palmic@email.cz>
-* @package LBox ubytovny-v-praze.cz
+* @package LBox
 * @version 1.0
 
 * @since 2007-12-08
