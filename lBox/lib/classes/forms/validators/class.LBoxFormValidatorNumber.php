501cb2f9e135379372653ab506293013b3e83836
Michal Palma <michal.palma@gmail.com>
2008-09-30 12:00:28 +0200

    Doplneni formularu

---
+++ lBox/lib/classes/forms/validators/class.LBoxFormValidatorNumber.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * zkontroluje, jestli je hodnota cislo
+ */
+class LBoxFormValidatorNumber extends LBoxFormValidator
+{
+	public function validate(LBoxFormControl $control = NULL) {
+		try {
+			if (strlen($control->getValue()) > 0)
+			if (!is_numeric($control->getValue())) {
+				throw new LBoxExceptionFormValidator(	LBoxExceptionFormValidator::MSG_FORM_VALIDATION_CONTROL_NOT_VALID,
+														LBoxExceptionFormValidator::CODE_FORM_VALIDATION_CONTROL_NOT_VALID);
+			}
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+}
+?>
