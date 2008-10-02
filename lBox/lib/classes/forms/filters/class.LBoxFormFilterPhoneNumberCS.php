1f6304b2e14964585e02d587e6ea2a4d0741cdb9
Michal Palma <michal.palma@gmail.com>
2008-10-02 13:42:35 +0200

    update forms

---
+++ lBox/lib/classes/forms/filters/class.LBoxFormFilterPhoneNumberCS.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * filtr pro pripadne doplneni predvolby ceskeho telefonniho cisla
+ */
+class LBoxFormFilterPhoneNumberCS extends LBoxFormFilter
+{
+	/**
+	 * regularni vyraz pro zjisteni, jestli
+	 * @var string
+	 */
+	protected $regPhone	= "^([[:digit:]]{9})$";
+	
+	/**
+	 * predvolba pro konkretni zemi
+	 * @var unknown_type
+	 */
+	protected $phoneNumberPrefix	= "+420";
+	
+	public function filter(LBoxFormControl $control = NULL) {
+		try {
+			if (ereg($this->regPhone, $control->getValue())) {
+				return $this->phoneNumberPrefix . $control->getValue();
+			}
+			return $control->getValue();
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+}
+?>
