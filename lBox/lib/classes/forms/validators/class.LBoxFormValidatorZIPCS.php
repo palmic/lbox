683a1c8d0a625a164c20265fb9f4c1e8e8281aa8
Michal Palma <michal.palma@gmail.com>
2008-10-27 15:36:47 +0100

    update forms

---
+++ lBox/lib/classes/forms/validators/class.LBoxFormValidatorZIPCS.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * zkontroluje, jestli je hodnota ceske PSC
+ */
+class LBoxFormValidatorZIPCS extends LBoxFormValidator
+{
+	/**
+	 * reg pattern kontroly PSC
+	 */
+	protected $reg	= "^[[:digit:]]{5}$";
+	
+	public function validate(LBoxFormControl $control = NULL) {
+		try {
+			if (strlen($control->getValue()) > 0)
+			if (!ereg($this->reg, $control->getValue())) {
+				throw new LBoxExceptionFormValidator(	LBoxExceptionFormValidator::MSG_FORM_VALIDATION_CONTROL_NOT_VALID,
+														LBoxExceptionFormValidator::CODE_FORM_VALIDATION_CONTROL_NOT_VALID);
+			}
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+}
+?>
