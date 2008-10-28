df1806952cb7a89e08ad7e25f4895e3e50ce1748
Michal Palma <michal.palma@gmail.com>
2008-10-28 11:34:39 +0100

    oprava form exception

--- lBox/lib/classes/exceptions/class.LBoxExceptionForm.php
+++ lBox/lib/classes/exceptions/class.LBoxExceptionForm.php
@@ -10,10 +10,12 @@ class LBoxExceptionForm extends LBoxException
 	const CODE_FORM_DATA_INVALID			= 15101;
 	const CODE_FORM_CONTROL_DOESNOT_EXISTS	= 15102;
 	const CODE_FORM_DUPLICATE_FORMNAME		= 15103;
+	const CODE_FORM_PROCESSOR_DOESNOT_EXISTS= 15104;
 	
 	const MSG_FORM_DATA_INVALID				= "Form data invalid";
 	const MSG_FORM_CONTROL_DOESNOT_EXISTS	= "Form data invalid";
 	const MSG_FORM_DUPLICATE_FORMNAME		= "This form name is already used";
+	const MSG_FORM_PROCESSOR_DOESNOT_EXISTS	= "No processor defined";
 	
 	protected $formArray				= array();
 
