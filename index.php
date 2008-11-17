ba995c030ff9c24b224e14b7253eed8cd12a81af
Michal Palma <michal.palma@gmail.com>
2008-11-17 15:40:46 +0100

    Opraveno nastaveni cesty k I18N templates

--- index.php
+++ index.php
@@ -1,15 +1,13 @@
 <?php
-
 require("lBox/lib/loader.php");
 
-/*var_dump(LBOX_REQUEST_URL);
-die();*/
 session_start();
 ob_start();
 
 //DbControl::$debug = true;
 
 //LBoxLoaderConfig::getInstance()->debug = true;
+
 DEFINE("LBOX_PATH_TEMPLATES_PAGES",			LBoxConfigSystem::getInstance()->getParamByPath("pages/templates/path"));
 DEFINE("LBOX_PATH_TEMPLATES_COMPONENTS",	LBoxConfigSystem::getInstance()->getParamByPath("components/templates/path"));
 DEFINE("LBOX_PATH_TEMPLATES_LAYOUTS",		LBoxConfigSystem::getInstance()->getParamByPath("layouts/templates/path"));
@@ -19,27 +17,22 @@ try {
 	LBoxFront::run();
 }
 catch (Exception $e) {
-	if (strlen(stristr(LBOX_REQUEST_URL_HOST, "localhost")) < 1) {
-		echo "Na serveru nastala chyba!";
-	}
-	else {
-		echo "<hr />";
-	    echo "Exception code:  <font style='color:blue'>". $e->getCode() ."</font>";
-	    echo "<br />";
-	    echo "Exception message: <font style='color:blue'>". nl2br($e->getMessage()) ."</font>";
-	    echo "<br />";
-	    echo "Thrown by: '". $e->getFile() ."'";
-	    echo "<br />";
-	    echo "on line: '". $e->getLine() ."'.";
-	    echo "<br />";
-	    echo "<br />";
-	    echo "Stack trace:";
-	    echo "<br />";
-	    echo nl2br($e->getTraceAsString());
-	    echo "<hr />";
-	}
+    echo "<hr />";
+    echo "Exception code:  <font style='color:blue'>". $e->getCode() ."</font>";
+    echo "<br />";
+    echo "Exception message: <font style='color:blue'>". nl2br($e->getMessage()) ."</font>";
+    echo "<br />";
+    echo "Thrown by: '". $e->getFile() ."'";
+    echo "<br />";
+    echo "on line: '". $e->getLine() ."'.";
+    echo "<br />";
+    echo "<br />";
+    echo "Stack trace:";
+    echo "<br />";
+    echo nl2br($e->getTraceAsString());
+    echo "<hr />";
 }
 
 //die("zobrazuju aspon neco in: ". __FILE__ ." at line:  ". __LINE__);
-@ob_end_flush();
+ob_end_flush();
 ?>
