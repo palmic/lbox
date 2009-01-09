4a9e7fc1114df6144e80583eb55a7446bc94c168
Michal Palma <michal.palma@gmail.com>
2009-01-09 18:18:36 +0100

    Upravy nekterych bugu + pridany nektere validatory, nebo filtry etc

---
+++ lBox/lib/classes/forms/validators/class.LBoxFormValidatorURLHTTPHTTPS.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * zkontroluje, jestli je hodnota absolutni URL
+ */
+class LBoxFormValidatorURLHTTPHTTPS extends LBoxFormValidator
+{
+	/**
+	 * reg pattern kontroly URL
+	 */
+	protected $reg	= '(http(s?):\/\/|ftp:\/\/)*([[:alpha:]][-[:alnum:]]*[[:alnum:]])(\.[[:alpha:]][-[:alnum:]]*[[:alpha:]])+(/[[:alpha:]][-[:alnum:]]*[[:alnum:]])*(\/?)(/[[:alpha:]][-[:alnum:]]*\.[[:alpha:]]{3,5})?(\?([[:alnum:]][-_%[:alnum:]]*=[-_%[:alnum:]]+)(&([[:alnum:]][-_%[:alnum:]]*=[-_%[:alnum:]]+))*)?$';
+	
+	public function validate(LBoxFormControl $control = NULL) {
+		try {
+			if (strlen($control->getValue()) > 0)
+			if (!ereg($this->reg, $control->getValue(), $regs)) {
+				throw new LBoxExceptionFormValidator(	LBoxExceptionFormValidator::MSG_FORM_VALIDATION_CONTROL_NOT_VALID,
+														LBoxExceptionFormValidator::CODE_FORM_VALIDATION_CONTROL_NOT_VALID);
+			}
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+}
+?>
