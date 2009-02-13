2fc8cf586d13413e6cb316276859c1b8814343a6
Michal Palma <michal.palma@gmail.com>
2009-02-13 22:03:58 +0100

    Pridany defaultni filtry fomulare

---
+++ lBox/lib/classes/forms/filters/class.LBoxFormFilterLowerCase.php
@@ -0,0 +1,13 @@
+<?php
+class LBoxFormFilterLowerCase extends LBoxFormFilter
+{
+	public function filter(LBoxFormControl $control = NULL) {
+		try {
+			return strtolower($control->getValue());
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+}
+?>
---
+++ lBox/lib/classes/forms/filters/class.LBoxFormFilterUpperCase.php
@@ -0,0 +1,13 @@
+<?php
+class LBoxFormFilterUpperCase extends LBoxFormFilter
+{
+	public function filter(LBoxFormControl $control = NULL) {
+		try {
+			return strtoupper($control->getValue());
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+}
+?>
