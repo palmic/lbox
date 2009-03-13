2ef00d9c9446bba2e873a2da63369fdd5100a088
Michal Palma <michal.palma@gmail.com>
2009-03-13 11:21:21 +0100

    Pridan control choose one radio + prazdny processor

---
+++ lBox/lib/classes/forms/processors/class.LBoxFormProcessorNull.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * processor, ktery slouzi k uspokojeni formu pri odesilani, aniz by neco delal (form bez processoru vyhazuje vyjimku)
+ */
+class LBoxFormProcessorNull extends LBoxFormProcessor
+{
+	public function process() {
+		try {
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+}
+?>
