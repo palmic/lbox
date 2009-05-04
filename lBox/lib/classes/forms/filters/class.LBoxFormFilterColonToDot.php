6c831b0f2dded58cc1db6f036bf5ad156fae8d03
Michal Palma <michal.palma@gmail.com>
2009-05-04 22:09:04 +0200

    Pridan defaultni filter

---
+++ lBox/lib/classes/forms/filters/class.LBoxFormFilterColonToDot.php
@@ -0,0 +1,13 @@
+<?php
+class LBoxFormFilterColonToDot extends LBoxFormFilter
+{
+	public function filter(LBoxFormControl $control = NULL) {
+		try {
+			return str_replace(",", ".", $control->getValue());
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+}
+?>
