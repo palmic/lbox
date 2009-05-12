580da76564257c12aeddd1a3141602154178943a
Michal Palma <michal.palma@gmail.com>
2009-05-12 09:50:31 +0200

    Pridan novy validator form

---
+++ lBox/lib/classes/forms/filters/class.LBoxFormFilterPhoneNumberCSWithPreselection.php
@@ -0,0 +1,17 @@
+<?php
+class LBoxFormFilterPhoneNumberCSWithPreselection extends LBoxFormFilter
+{
+	public function filter(LBoxFormControl $control = NULL) {
+		try {
+			$out	= $control->getValue();
+			if (is_numeric($out) && strlen($out) == 9) {
+				$out	= "420$out";
+			}
+			return $out;
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+}
+?>
