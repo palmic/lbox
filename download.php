90968dc344500fa904d8c5ece931e6b3ef8e02f0
Michal Palma <michal.palma@gmail.com>
2009-05-20 23:18:37 +0200

    oprava download.php

--- download.php
+++ download.php
@@ -12,18 +12,18 @@ if (!($file = $files->current()) instanceof FilesRecord) {
 	die;
 }
 
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
+header("Content-type: Content-type");
+header("Content-Disposition: attachment; filename=". str_replace (" ", "_", $file->getFileName()));
+header("Content-Transfer-Encoding: binary");
+
 // zapocist stazeni
 if ($readed = @readfile($file->getFilePath())) {
 	$downloaded = new FilesDownloadedRecord();
 	$downloaded->ref_file	= $file->id;
 	$downloaded->countIn();
 }
-
-header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-header("Last-Modified: ".gmdate("D, d M Y H:i:s")." GMT");
-header("Content-type: Content-type");
-header("Content-Disposition: attachment; filename=". str_replace (" ", "_", $file->getFileName()));
-header("Content-Transfer-Encoding: binary");
 
 ob_end_flush();
 ?>
