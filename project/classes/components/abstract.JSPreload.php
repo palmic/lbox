a1ceaf82efcc87af9da7477e310c1bdc803ac607
Michal Palma <michal.palma@gmail.com>
2009-06-08 18:51:09 +0200

    pridana komponenta konvertujici richtext textarea na YUI richtext editor

---
+++ project/classes/components/abstract.JSPreload.php
@@ -0,0 +1,19 @@
+<?
+/**
+ * main menu class
+* @author Michal Palma <palmic@email.cz>
+* @package LBox
+* @version 1.0
+* @since 2009-06-08
+*/
+abstract class JSPreload extends LBoxComponent
+{
+	protected function executePrepend(PHPTAL $TAL) {
+		try {
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+}
+?>
---
+++ project/classes/components/class.JSPreloadWebAdmin.php
@@ -0,0 +1,12 @@
+<?
+/**
+ * main menu class
+* @author Michal Palma <palmic@email.cz>
+* @package LBox
+* @version 1.0
+* @since 2009-06-08
+*/
+class JSPreloadWebAdmin extends JSPreload
+{
+}
+?>
