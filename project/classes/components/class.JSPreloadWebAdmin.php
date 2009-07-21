32d249dd53047334943992c41432077b630209fd
Michal Palma <michal.palma@gmail.com>
2009-07-21 18:32:59 +0200

    Pridana moznost manualni definice stranek, ktere budou metanodum umoznovat resize

--- project/classes/components/class.JSPreloadWebAdmin.php
+++ project/classes/components/class.JSPreloadWebAdmin.php
@@ -8,5 +8,28 @@
 */
 class JSPreloadWebAdmin extends JSPreload
 {
+	/**
+	 * zjistuje, jestli je na aktualne zobrazene strance povolen resize
+	 * @return bool
+	 */
+	public function isAllowedResize() {
+		try {
+			$pagesAllowedResize		= LBoxConfigManagerProperties::getPropertyContentByName("metanodes_allow_resize_pages");
+			$pagesAllowedResizeArr	= explode(" ", $pagesAllowedResize);
+			foreach ($pagesAllowedResizeArr as $pageAllowedResize) {
+				$pageAllowedResize	= trim($pageAllowedResize);
+				switch (true) {
+					case strtolower($pageAllowedResize) == "all":
+					case $pageAllowedResize == LBoxFront::getPage()->id:
+							return true;
+						break;
+				}
+			}
+			return false;
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
 }
 ?>
