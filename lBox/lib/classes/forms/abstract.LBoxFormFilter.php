00b43013668c1cfe1b974d4c6274e3e6dc655305
Michal Palma <michal.palma@gmail.com>
2009-07-31 17:38:24 +0200

    Pridana moznost definicovat Form filtru jiny zdrojocy control, nez na ktery je povesen

--- lBox/lib/classes/forms/abstract.LBoxFormFilter.php
+++ lBox/lib/classes/forms/abstract.LBoxFormFilter.php
@@ -5,10 +5,38 @@
 abstract class LBoxFormFilter
 {
 	/**
+	 * optionaly control with source data - to pass data from one form control to another possibilities
+	 * @var LBoxFormControl
+	 */
+	protected $control;
+	
+	/**
+	 * to define another control as source data bearer 
+	 * @param LBoxFormControl $control
+	 * @return unknown_type
+	 */
+	public function __construct(LBoxFormControl $control = NULL) {
+		$this->control	= $control;
+	}
+
+	/**
 	 *
 	 * @param LBoxFormControl control 
 	 * @return string
 	 */
 	abstract public function filter(LBoxFormControl $control = NULL);
+	
+	/**
+	 * getter for source data control if defined
+	 * @return LBoxFormControl
+	 */
+	public function getControl() {
+		try {
+			return $this->control;
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
 }
 ?>
---
+++ lBox/lib/classes/forms/filters/class.LBoxFormFilterURLStringFromName.php
@@ -0,0 +1,13 @@
+<?php
+class LBoxFormFilterURLStringFromName extends LBoxFormFilter
+{
+	public function filter(LBoxFormControl $control = NULL) {
+		try {
+			return LBoxUtil::getURLByNameString(trim($control->getValue()));
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+}
+?>
