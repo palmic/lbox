58bd7ae939ae7bfe5b79986478adb44b359ccb60
Michal Palma <michal.palma@gmail.com>
2010-01-10 18:23:26 +0100

    Pridan na vystup standardni gzip handler ob_gzhandler

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 require("lBox/lib/loader.php");
 
 session_start();
-ob_start();
+if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) {
+	ob_start("ob_gzhandler");
+}
+else ob_start();
 
 //DbControl::$debug = true;
 //LBoxCache::$debug = true;
