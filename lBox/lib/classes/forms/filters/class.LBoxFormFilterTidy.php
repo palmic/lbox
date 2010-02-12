6af41d291ddcf7ad659a6bbfc9f9440e8fc1233d
Michal Palma <palmic@linux-2fl4.site>
2010-02-12 16:55:30 +0100

    drobne updaty forms

---
+++ lBox/lib/classes/forms/filters/class.LBoxFormFilterTidy.php
@@ -0,0 +1,26 @@
+<?php
+class LBoxFormFilterTidy extends LBoxFormFilter
+{
+	public function filter(LBoxFormControl $control = NULL) {
+		try {
+			$value	= $control->getValue();
+			if (class_exists("tidy")) {
+				$tidyConfig = array('indent' => true,
+									'output-xml' => false,
+									'output-html' => false,
+									'output-xhtml' => true,
+									'show-body-only' => true,
+									'wrap' => 200);
+				$tidy	= new tidy();
+				//var_dump($content);
+				$value = $tidy->repairString($value, $tidyConfig/*, 'UTF8'*/);
+				//var_dump($content);die;
+			}
+			return $value;
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+}
+?>
