a66aa89fd90afe7a7fb3ccbd7cd80aa39173c41c
Michal Palma <palmic@linux-pm48.site>
2010-02-18 21:00:59 +0100

    drobna zmena v OFArticle

--- project/classes/filters/output/class.OutputFilterArticle.php
+++ project/classes/filters/output/class.OutputFilterArticle.php
@@ -4,9 +4,9 @@
 * @package LBox softub.cz
 * @version 1.0
 * @license http://creativecommons.org/licenses/by-sa/3.0/
-* @since 2007-12-08
+* @since 2010-02-12
 */
-class OutputFilterArticle extends LBoxOutputFilter
+class OutputFilterArticle extends OutputFilterRecordEditableByAdmin
 {
 	/**
 	 * name of config attribute referencing article displaying page via ID
@@ -14,35 +14,15 @@ class OutputFilterArticle extends LBoxOutputFilter
 	 */
 	protected $configVarNameArticleRefPage 	= "ref_page_article";
 
-	/**
-	 * article ref page class
-	 * @var string
-	 */
-	protected $articleRefPageClass 			= "PageArticle";
-
-	/**
-	 * cache funkce
-	 * @var bool
-	 */
-	protected $isLastHeadlight;
-
 	public function prepare($name = "", $value = NULL) {
 		try {
 			switch ($name) {
-				case "id":
-					return $this->instance->getParamDirect("url");
-					break;
 				case "url":
-					$refPageAttName				= $this->configVarNameArticleRefPage;
-					// najdeme stranku zobrazovani clanku podle reference
-					$idPageItem					= LBoxConfigManagerProperties::getInstance()->getPropertyByName($refPageAttName)->getContent();
-					$pageItem		 			= LBoxConfigManagerStructure::getInstance()->getPageById($idPageItem);
-					$pageClass 					= $pageItem->class;
-					if ($pageClass !== $this->articleRefPageClass) {
-						throw new LBoxExceptionConfigStructure("Referenced page (id=$idPagePhotogallery) defined in properties.xml like '$refPageAttName' is not '". $this->articleRefPageClass ."' type, but '$pageClass'! Check it in structure config.");
-					}
-					$urlBase	= $this->instance->getParamDirect("url");
-					return $pageItem->url .":$urlBase";
+					$pageItem = LBoxConfigManagerStructure::getInstance()->getPageById(LBoxConfigManagerProperties::gpcn($this->configVarNameArticleRefPage));
+					return  LBoxUtil::getURLWithParams(array($this->instance->getParamDirect("url")), $pageItem->url);
+					break;
+				case "url_param":
+					return  $this->instance->getParamDirect("url");
 					break;
 				case "urlAbsolute":
 					return LBOX_REQUEST_URL_SCHEME ."://". LBOX_REQUEST_URL_HOST . $this->prepare("url");
@@ -51,80 +31,20 @@ class OutputFilterArticle extends LBoxOutputFilter
 					return htmlentities($this->prepare("heading"));
 					break;
 				case "publishedDate":
-					return date("j.n. Y", LBoxUtil::getDateTimeStamp($this->instance->published));
+					return date("j.n. Y", $this->instance->time_published);
 					break;
 				case "published2":
-					return date("j.n.Y | H:i:s", LBoxUtil::getDateTimeStamp($this->instance->published));
+					return date("j.n.Y | H:i:s", $this->instance->time_published);
 					break;
-				case "isLastHeadlight":
-					return $this->isLastHeadlight();
-					break;
-				case "listIcon":
-					return $this->getListIconURL();
-					break;
-				case "listIconDimensions":
-					return array(
-								"x"	=> LBoxConfigManagerProperties::getPropertyContentByName("article_image_title_width"),
-								"y"	=> LBoxConfigManagerProperties::getPropertyContentByName("article_image_title_height"),
-								);
+				case "perex_raw":
+					return strip_tags($this->instance->perex);
 					break;
 				default:
-					return $value;
+					return parent::prepare($name, $value);
 			}
 		}
 		catch (Exception $e) {
 			throw $e;
-		}
-	}
-
-	/**
-	 * vraci, jestli je clanek posledni headlight
-	 * @return bool
-	 */
-	protected function isLastHeadlight() {
-		try {
-			if (is_bool($this->isLastHeadlight)) {
-				return $this->isLastHeadlight;
-			}
-			$className				= $this->instance->getClassVar("itemsType");
-			$articlesHeadlight 		= new $className(false, array("headlight" => 0, "published" => 0), array(0, 1));
-			return $this->isLastHeadlight = ($this->instance->getParamDirect("url") == $articlesHeadlight->current()->url);
-		}
-		catch (Exception $e) {
-			throw $e;
-		}
-	}
-
-	/**
-	 * vraci url na ikonu pokud nejaka u clanku je
-	 * @return string
-	 */
-	protected function getListIconURL() {
-		try {
-			// custom icon
-			if ($this->instance->getParamDirect("ref_list_icon") < 1) {
-				if ($this->instance->getImageTitle()) {
-					return $this->instance->getImageTitle()->url;
-				}
-			}
-			$urlPath	= LBoxConfigManagerProperties::getInstance()->getPropertyByName("icon_url_path")->getContent();
-			if ($this->instance->getParamDirect("ref_list_icon") < 1) {
-				$records	= new ListIconsRecords(false, array("default" => 0, "id" => 0), array(0, 1));
-				$record		= $records->current();
-			}
-			else {
-				$record		= new ListIconsRecord($this->instance->getParamDirect("ref_list_icon"));
-			}
-			return "$urlPath/". $record->filename .".". $record->ext;
-		}
-		catch (Exception $e) {
-			switch ($e->getCode()) {
-				case LBoxExceptionFilesystem::CODE_FILE_NOT_EXISTS:
-						return "image not found";
-					break;
-				default:
-					throw $e;
-			}
 		}
 	}
 }
