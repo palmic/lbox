391e1a876b504e03d90194ba4bd5090cac806f8c
Michal Palma <palmic@linux-pm48.site>
2010-02-19 18:29:54 +0100

    pridan OFPhoto

---
+++ project/classes/filters/output/class.OutputFilterPhoto.php
@@ -0,0 +1,30 @@
+<?
+/**
+* @author Michal Palma <palmic@email.cz>
+* @package LBox
+* @version 1.0
+* @since 2010-02-19
+*/
+class OutputFilterPhoto extends LBoxOutputFilter
+{
+	/**
+	 * nazev property s cestou k adresari s fotkama
+	 * @var string
+	 */
+	protected $propertyNamePath = "";
+	
+	public function prepare($name = "", $value = NULL) {
+		switch ($name) {
+			case "url":
+					$path	= $this->instance->getFilePath();
+					$path	= str_replace("<project>", LBOX_PATH_PROJECT, $path);
+					$path	= str_replace(LBOX_PATH_PROJECT, "", $path);
+					return $path;
+				break;
+			default:
+					return $value;
+				break;
+		}
+	}
+}
+?>
