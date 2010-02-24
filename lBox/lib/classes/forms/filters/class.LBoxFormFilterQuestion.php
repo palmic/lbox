047dd5ed44ddaf838cc79439d194d956a7d85006
Michal Palma <palmic@linux-pm48.site>
2010-02-24 18:23:49 +0100

    drobne upravy ve forms

---
+++ lBox/lib/classes/forms/filters/class.LBoxFormFilterQuestion.php
@@ -0,0 +1,15 @@
+<?php
+class LBoxFormFilterQuestion extends LBoxFormFilter
+{
+	public function filter(LBoxFormControl $control = NULL) {
+		try {
+			$value	 = $control->getValue();
+			$value	.= (substr($value, strlen($value)-1) == "?") ? "" : "?";
+			return ucfirst($value);
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+}
+?>
