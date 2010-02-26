9e88b1008e573fcf029999c1807e92a7bf56ad59
Michal Palma <palmic@linux-pm48.site>
2010-02-26 14:59:32 +0100

    update forms

---
+++ lBox/lib/classes/forms/filters/class.LBoxFormFilterCopyURLFromName.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * copy URL value from name control
+ */
+class LBoxFormFilterCopyURLFromName extends LBoxFormFilterCopyValue
+{
+	public function filter(LBoxFormControl $control = NULL) {
+		try {
+			return LBoxUtil::getURLByNameString(trim(parent::filter($control)));
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+}
+?>
---
+++ lBox/lib/classes/forms/filters/class.LBoxFormFilterCopyValue.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * copy value from src control
+ */
+class LBoxFormFilterCopyValue extends LBoxFormFilter
+{
+	/**
+	 * src value control
+	 * @var LBoxFormControl
+	 */
+	protected $controlSRC;
+	
+	public function filter(LBoxFormControl $control = NULL) {
+		try {
+			if (!$this->controlSRC instanceof LBoxFormControl) {
+				throw new LBoxExceptionFormValidator("Bad instance var", LBoxExceptionFormValidator::CODE_BAD_INSTANCE_VAR);
+			}
+			//return (strlen($control->getValue()) < 1) ? $this->controlSRC->getValue() : $control->getValue();
+			return $this->controlSRC->getValue();
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+	
+	/**
+	 * source value control setter
+	 * @param LBoxFormControl $control
+	 */
+	public function setControlSRC(LBoxFormControl $control) {
+		try {
+			$this->controlSRC	= $control;
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+}
+?>
