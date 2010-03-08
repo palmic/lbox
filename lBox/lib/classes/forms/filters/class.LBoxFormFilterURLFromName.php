692e62a17d991b7cdda840df7cecd2ddd29cf62b
Michal Palma <palmic@linux-eopg.site>
2010-03-08 14:52:34 +0100

    novy validator

---
+++ lBox/lib/classes/forms/filters/class.LBoxFormFilterURLFromName.php
@@ -0,0 +1,36 @@
+<?php
+class LBoxFormFilterURLFromName extends LBoxFormFilter
+{
+	protected $controlName;
+	
+	public function filter(LBoxFormControl $control = NULL) {
+		try {
+			if (!$this->controlName instanceof LBoxFormControl) {
+				throw new LBoxExceptionFormValidator("Bad instance var", LBoxExceptionFormValidator::CODE_BAD_INSTANCE_VAR);
+			}
+			if (strlen($control->getValue()) < 1) {
+				return LBoxUtil::getURLByNameString(trim($this->controlName->getValue()));
+			}
+			else {
+				return $control->getValue();
+			}
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+	
+	/**
+	 * setter controlu nazvu
+	 * @param LBoxFormControl $control
+	 */
+	public function setControlName(LBoxFormControl $control) {
+		try {
+			$this->controlName	= $control;
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+}
+?>
