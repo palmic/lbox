d1a02cef093ca4577e559c988a7cba16f7be9f66
Michal Palma <michal.palma@gmail.com>
2010-04-13 11:03:44 +0200

    uprava LBF filteru

--- lBox/lib/classes/forms/filters/class.LBoxFormFilterQuestion.php
+++ lBox/lib/classes/forms/filters/class.LBoxFormFilterQuestion.php
@@ -4,7 +4,7 @@ class LBoxFormFilterQuestion extends LBoxFormFilter
 	public function filter(LBoxFormControl $control = NULL) {
 		try {
 			$value	 = $control->getValue();
-			$value	.= (substr($value, strlen($value)-1) == "?") ? "" : "?";
+			//$value	.= (substr($value, strlen($value)-1) == "?") ? "" : "?";
 			return ucfirst($value);
 		}
 		catch (Exception $e) {
