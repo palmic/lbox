6036afe2a2888a77a32f9608f71b8b609d514c7c
Michal Palma <michal.palma@gmail.com>
2010-07-24 19:50:29 +0200

    Validace required pro LBFormControlsMultiple pro vsechny/jeden subcontrol musi byt vyplnen

---
+++ lBox/lib/classes/forms/validators/class.LBoxFormValidatorRequiredMultiAll.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * kontroluje jestli jsou vsechny subcontrols v multicontrolu vyplneny
+ */
+class LBoxFormValidatorRequiredMultiAll extends LBoxFormValidatorRequired
+{
+	public function validate(LBoxFormControl $control = NULL) {
+		try {
+			foreach ($control->getControls() as $control) {
+				parent::validate($control);
+			}
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+}
+?>
---
+++ lBox/lib/classes/forms/validators/class.LBoxFormValidatorRequiredMultiOne.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * kontroluje jestli alespon jeden subcontrol v multicontrolu je vyplnen
+ */
+class LBoxFormValidatorRequiredMultiOne extends LBoxFormValidatorRequired
+{
+	public function validate(LBoxFormControl $control = NULL) {
+		try {
+			foreach ($control->getControls() as $control) {
+				try {
+					parent::validate($control);
+				}
+				catch (LBoxExceptionFormValidator $e) {
+					// jina vyjimka nez empty control
+					if ($e->getCode() != LBoxExceptionFormValidator::CODE_FORM_VALIDATION_CONTROL_EMPTY) {
+						throw $e;
+					}
+					else {
+						// tento neprosel - dalsi control
+						continue;
+					}
+				}
+				// parent usoudil, ze control je vyplnen - OK jeden staci
+				return;
+			}
+			// pokud dojde az sem (nenasel jedinej vyplnenej) vyhodi posledni required vyjimku (validace neprosla)
+			throw $e;
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
+}
+?>
