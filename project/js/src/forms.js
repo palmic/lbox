cd82251547ae166146bf68fd21c77115affc77c1
Michal Palma <michal.palma@gmail.com>
2010-07-25 21:09:23 +0200

    Opraveno ovladani subselectu v CSOB LMS forms.js checkboxy choose more

--- project/js/src/forms.js
+++ project/js/src/forms.js
@@ -21,20 +21,30 @@ lbox_forms_input_deactivate_classname = function(input) {
 }
 lbox_forms_show	= function(control) {
 	control.getElements('input').each(function(input, index) {
-		input.disabled = false;
+		if (input.retrieve('disabledBefore')) {
+			input.disabled = true;
+		}
+		else {
+			input.disabled = false;
+		}
 	});
 	control.getElements('select').each(function(input, index) {
-		input.disabled = false;
+		if (input.retrieve('disabledBefore')) {
+			input.disabled = true;
+		}
+		else {
+			input.disabled = false;
+		}
 	});
 	/*if (Fx && Fx.Morph) {
-		if (control.sizeOri.y) {
+		if (control.retrieve('sizeOri').y) {
 			if (control.getStyle('height').toInt() == 0) {
 				control.setStyle('height', 0);
 				control.setStyle('display', 'block');
-				if (!control.fxmorph) {
-					control.fxmorph = new Fx.Morph(control.id, {duration: 'short', transition: Fx.Transitions.Sine.easeOut});
+				if (!control.retrieve('fxmorph')) {
+					control.store('fxmorph', new Fx.Morph(control.id, {duration: 'short', transition: Fx.Transitions.Sine.easeOut}));
 				}
-				control.fxmorph.start({'height': [0, control.sizeOri.y]}).chain(
+				control.retrieve('fxmorph').start({'height': [0, control.retrieve('sizeOri').y]}).chain(
 						function(){ this.start({'background-color': ['#FFFFFF', '#80B0FF']}); },
 						function(){ this.start({'background-color': ['#80B0FF', '#FFFFFF']}); },
 						function(){ this.start({'background-color': ['#FFFFFF', '#80B0FF']}); },
@@ -65,6 +75,14 @@ lbox_forms_hide	= function(control, calledByChange) {
 					input.value	= '';
 			}
 		}
+		if (input.retrieve('disabledBefore') == undefined) {
+			if (input.disabled) {
+				input.store('disabledBefore', true);
+			}
+			else {
+				input.store('disabledBefore', false);
+			}
+		}
 		input.disabled = true;
 		lbox_forms_input_deactivate_classname(input);
 	});
@@ -72,18 +90,26 @@ lbox_forms_hide	= function(control, calledByChange) {
 		if (calledByChange) {
 			input.value	= '';
 		}
+		if (input.retrieve('disabledBefore') == undefined) {
+			if (input.disabled) {
+				input.store('disabledBefore', true);
+			}
+			else {
+				input.store('disabledBefore', false);
+			}
+		}
 		input.disabled = true;
 		lbox_forms_input_deactivate_classname(input);
 	});
 	/*if (Fx && Fx.Morph) {
-		control.sizeOri = control.getSize();
+		control.store('sizeOri', control.getSize());
 		control.setStyle('overflow', 'hidden');
 		if (control.getStyle('height').toInt() > 0) {
 			control.setStyle('height', control.getStyle('height').toInt());
-			if (!control.fxmorph) {
-				control.fxmorph = new Fx.Morph(control.id, {duration: 'short', transition: Fx.Transitions.Sine.easeOut});
+			if (!control.retrieve('fxmorph')) {
+				control.store('fxmorph', new Fx.Morph(control.id, {duration: 'short', transition: Fx.Transitions.Sine.easeOut}));
 			}
-			control.fxmorph.start({'height': [control.sizeOri.y, 0]});
+			control.retrieve('fxmorph').start({'height': [control.retrieve('sizeOri').y, 0]});
 		}
 		else {
 			control.setStyle('height', '0px');
@@ -144,7 +170,21 @@ lbox_forms_switch_slaves_by_master = function(masterInput, calledByChange) {
 													lbox_forms_show(control);
 												}
 												else {
-													lbox_forms_hide(control, calledByChange);
+													/* pred zhasnutim controlu ovladaneho checkboxy je treba checknout, jestli neni checked jeste jiny, ktery ho udrzuje zapnuty */
+													var foundSibling = false;
+													masterControl.getElements('input').each(function(masterInputSibling, index) {
+														if (masterInputSibling.checked) {
+															/* check pokud je v mastervalues */
+															for (i in masterValues) {
+																if (masterValues[i] == masterInputSibling.id.substring(masterInputSibling.id.indexOf(masterCTRLIDSimple)+masterCTRLIDSimple.length+1)) {
+																	foundSibling = true;
+																}
+															}
+														}
+													});
+													if (!foundSibling) {
+														lbox_forms_hide(control, calledByChange);
+													}
 												}
 											}
 										break;
@@ -219,63 +259,69 @@ lbox_forms_switch_slaves_by_master = function(masterInput, calledByChange) {
 }
 window.addEvent('domready', function() {
 	$$('.control').each(function(control, index) {
-		if (!control.hasClass('control-multiple')) {
-			control.getElements('input').each(function(input, index) {
-				input.addEvent('change', function() {
-					lbox_forms_switch_slaves_by_master(this, true);
-					lbox_forms_input_activate_classname(this);
-				});
-				/* kvuli IEckum je treba nastavit protekani eventu click->focus->change */
-				input.addEvent('focus', function() {this.fireEvent('change');return false;});
-				input.addEvent('click', function() {this.fireEvent('focus')});
-				switch (input.type) {
-					case "text":
-						if (input.value.length > 0) {
-							lbox_forms_input_activate_classname(input);
+		if (!control.getParent('form').hasClass('disabled')) {
+			if (!control.hasClass('control-multiple')) {
+				control.getElements('input').each(function(input, index) {
+					if (input.retrieve('disabledBefore') == undefined) {
+						if (input.disabled) {
+							input.store('disabledBefore', true);
 						}
-					break;
-					case "radio":
-						if (input.checked) {
-							lbox_forms_input_activate_classname(input);
+						else {
+							input.store('disabledBefore', false);
 						}
-					break;
-					case "checkbox":
-						if (input.checked) {
-							lbox_forms_input_activate_classname(input);
-						}
-					break;
-				}
-				lbox_forms_switch_slaves_by_master(input);
-			});
-			control.getElements('select').each(function(input, index) {
-				input.addEvent('change', function() {
-					lbox_forms_switch_slaves_by_master(this, true);
-					lbox_forms_input_activate_classname(this);
-				});
-				if (isIE()) {
+					}
+					input.addEvent('change', function() {
+						lbox_forms_switch_slaves_by_master(this, true);
+						lbox_forms_input_activate_classname(this);
+					});
 					/* kvuli IEckum je treba nastavit protekani eventu click->focus->change */
-					input.addEvent('focus', function() {this.fireEvent('change');return false;});
-					input.addEvent('click', function() {this.fireEvent('focus')});
-				}
-				switch (input.type) {
-					case "text":
-						if (input.value.length > 0) {
-							lbox_forms_input_activate_classname(input);
+					if (isIE()) {
+						input.addEvent('focus', function() {this.fireEvent('change');return false;});
+						input.addEvent('click', function() {this.fireEvent('focus')});
+					}
+					switch (input.type) {
+						case "text":
+							if (input.value.length > 0) {
+								lbox_forms_input_activate_classname(input);
+							}
+						break;
+						case "radio":
+							if (input.checked) {
+								lbox_forms_input_activate_classname(input);
+							}
+						break;
+						case "checkbox":
+							if (input.checked) {
+								lbox_forms_input_activate_classname(input);
+							}
+						break;
+					}
+					lbox_forms_switch_slaves_by_master(input);
+				});
+				control.getElements('select').each(function(input, index) {
+					if (input.retrieve('disabledBefore') == undefined) {
+						if (input.disabled) {
+							input.store('disabledBefore', true);
 						}
-					break;
-					case "radio":
-						if (input.checked) {
-							lbox_forms_input_activate_classname(input);
+						else {
+							input.store('disabledBefore', false);
 						}
-					break;
-					case "checkbox":
-						if (input.checked) {
-							lbox_forms_input_activate_classname(input);
-						}
-					break;
-				}
-				lbox_forms_switch_slaves_by_master(input);
-			});
+					}
+					input.addEvent('change', function() {
+						lbox_forms_switch_slaves_by_master(this, true);
+						lbox_forms_input_activate_classname(this);
+					});
+					if (isIE()) {
+						/* kvuli IEckum je treba nastavit protekani eventu click->focus->change */
+						input.addEvent('focus', function() {this.fireEvent('change');return false;});
+						input.addEvent('click', function() {this.fireEvent('focus')});
+					}
+					if (input.value.length > 0) {
+						lbox_forms_input_activate_classname(input);
+					}
+					lbox_forms_switch_slaves_by_master(input);
+				});
+			}			
 		}
 	});
 });
