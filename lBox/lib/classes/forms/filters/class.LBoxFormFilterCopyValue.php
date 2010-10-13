ef5103fddac90c467ba607c412d077a8012530b0
Michal Palma <michal.palma@gmail.com>
2010-10-13 10:37:37 +0200

    update filteru copy value

--- lBox/lib/classes/forms/filters/class.LBoxFormFilterCopyValue.php
+++ lBox/lib/classes/forms/filters/class.LBoxFormFilterCopyValue.php
@@ -10,13 +10,23 @@ class LBoxFormFilterCopyValue extends LBoxFormFilter
 	 */
 	protected $controlSRC;
 	
+	/**
+	 * flag
+	 * @var bool
+	 */
+	protected $copyOnlyEmpty = false;
+	
 	public function filter(LBoxFormControl $control = NULL) {
 		try {
 			if (!$this->controlSRC instanceof LBoxFormControl) {
 				throw new LBoxExceptionFormValidator("Bad instance var", LBoxExceptionFormValidator::CODE_BAD_INSTANCE_VAR);
 			}
-			//return (strlen($control->getValue()) < 1) ? $this->controlSRC->getValue() : $control->getValue();
-			return $this->controlSRC->getValue();
+			if ($this->copyOnlyEmpty) {
+				return (strlen($control->getValue()) < 1) ? $this->controlSRC->getValue() : $control->getValue();
+			}
+			else {
+				return $this->controlSRC->getValue();
+			}
 		}
 		catch (Exception $e) {
 			throw $e;
@@ -32,6 +42,20 @@ class LBoxFormFilterCopyValue extends LBoxFormFilter
 			$this->controlSRC	= $control;
 		}
 		catch (Exception $e) {
+			throw $e;
+		}
+	}
+
+	/**
+	 * does copy only if target control is not already filled
+	 * @param bool $value
+	 * @throws LBoxExceptionFormValidator
+	 */
+	public function setCopyOnlyEmpty($value = true) {
+		try {
+			$this->copyOnlyEmpty	= $value;
+		}
+		catch(Exception $e) {
 			throw $e;
 		}
 	}
