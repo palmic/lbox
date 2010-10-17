a5608eed97cbad711fd04d7b70f69be1d10d4ff1
Michal Palma <michal.palma@gmail.com>
2010-10-17 16:03:09 +0200

    U metanodes int a string nahrazeny blokove div za span kvuli validite a na vystupu odstranovany html tagy + umozneno ziskavat metanodes v sablonach opakovane v ruznych typech

--- lBox/lib/classes/front/metanodes/class.LBoxMetanodeString.php
+++ lBox/lib/classes/front/metanodes/class.LBoxMetanodeString.php
@@ -12,5 +12,22 @@ class LBoxMetanodeString extends LBoxMetanode
 	CONST XT_FORM_CTRL_CLASSNAME			= "LBoxFormControlFill";
 	CONST XT_FORM_CTRL_TEMPLATE_FILENAME	= "metanode_string.html";
 	CONST TEMPLATE_FILENAME					= "metanode_string.html";
+	
+	/**
+	 * pretizeno o cisteni elementu
+	 */
+	public function getContent() {
+		try {
+			if (get_class() == get_class($this)) {
+				return strip_tags(parent::getContent());
+			}
+			else {
+				return parent::getContent();
+			}
+		}
+		catch (Exception $e) {
+			throw $e;
+		}
+	}
 }
 ?>
